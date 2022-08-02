e0f88cb3592bab7a74b2e558bfc2a6a0415cfc5f
Yujing Wu <ywu@esri.com>
2022-08-02 17:43:02 -0400

    Updated Regex Text Adapter for GeoEvent 11.0
    Deleted obsolete log4j properties file
    Modified .gitignore to ignore compiled artifacts
    Modernized logging strategy and formatted Java files
    Replaced spatial reference fields and added Locale for number formatter
    field

--- geoevent-components/solutions-geoevent/adapters/regexText-adapter/src/main/java/com/esri/geoevent/solutions/adapter/regexText/RegexTextInboundAdapter.java
+++ geoevent-components/solutions-geoevent/adapters/regexText-adapter/src/main/java/com/esri/geoevent/solutions/adapter/regexText/RegexTextInboundAdapter.java
@@ -25,91 +25,102 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
-import com.esri.ges.adapter.text.TextInboundAdapter;
 import com.esri.ges.adapter.AdapterDefinition;
+import com.esri.ges.adapter.text.TextInboundAdapter;
 import com.esri.ges.core.component.ComponentException;
-
+import com.esri.ges.framework.i18n.BundleLogger;
+import com.esri.ges.framework.i18n.BundleLoggerFactory;
 
 public class RegexTextInboundAdapter extends TextInboundAdapter
 {
-	// This class enables an attribute separator to be defined as a regular expression (which the base class does not handle).
-	// It swaps all matches of this regular expression in the incoming stream with a single-character delimiter (which the base class will handle)
-	// It then passes the altered stream to the base class to handle as normal
-	static final private Log log = LogFactory.getLog(RegexTextInboundAdapter.class);
-	private String attributeSeparator;
-	protected static String ATTRIBUTE_SEPARATOR_TO_BE_REPLACED_PROPERTY_NAME = "AttributeSeparatorToBeReplaced";
-	private String separatorToBeReplaced = "";
-	private static Charset charset = Charset.forName("UTF-8");
-	
-	public RegexTextInboundAdapter(AdapterDefinition adapterDefinition)
-			throws ComponentException
-	{
-		super(adapterDefinition);
-	}
+  // This class enables an attribute separator to be defined as a regular
+  // expression (which the base class does not handle).
+  // It swaps all matches of this regular expression in the incoming stream with a
+  // single-character delimiter (which the base class will handle)
+  // It then passes the altered stream to the base class to handle as normal
+  static final private BundleLogger LOGGER                                           = BundleLoggerFactory.getLogger(RegexTextInboundAdapter.class);
+  private String                    attributeSeparator;
+  protected static String           ATTRIBUTE_SEPARATOR_TO_BE_REPLACED_PROPERTY_NAME = "AttributeSeparatorToBeReplaced";
+  private String                    separatorToBeReplaced                            = "";
+  private static Charset            charset                                          = Charset.forName("UTF-8");
 
-	@Override
-	public void afterPropertiesSet()
-	{
-		super.afterPropertiesSet();
-		// grab references to two properties in the definition that will be used to adapt the incoming bytes
-		if( hasProperty(ATTRIBUTE_SEPARATOR_TO_BE_REPLACED_PROPERTY_NAME) )
-			separatorToBeReplaced = (String)getProperty(ATTRIBUTE_SEPARATOR_TO_BE_REPLACED_PROPERTY_NAME).getValue();
-		if( hasProperty(ATTRIBUTE_SEPARATOR_PROPERTY_NAME) )
-			attributeSeparator = (String)getProperty(ATTRIBUTE_SEPARATOR_PROPERTY_NAME).getValue();
-	}
-	
-	@Override
-	public void receive(ByteBuffer buf, String str)
-	{
-		ByteBuffer bb = buf;
-		// Check first whether we need to do anything different from the out-of-the-box TextAdapter
-		if(StringUtils.isNotBlank(separatorToBeReplaced))
-		{
-			// Decode the buffer
-			CharSequence charseq = decode(buf);
-			// Replace the attribute separator with a simple one
-			log.debug("Replacing separator '" + separatorToBeReplaced + "' with '" + attributeSeparator + "'");
-			Pattern p = Pattern.compile(separatorToBeReplaced);
-			Matcher m = p.matcher(charseq);
-			String newString = m.replaceAll(attributeSeparator);
-			// Put up some debugging info
-			if (newString.length() >= 300)
-				log.debug(newString.substring(0, 300) + "...---..." + newString.substring(newString.length() - 300));
-			else
-				log.debug(newString);
-			// Re-encode the string into a byte buffer
-			bb = encode(newString);
-		}
-		// Let the base class now handle the input as normal
-		super.receive(bb, str);
-	}
-	
-	private CharSequence decode(ByteBuffer buf)
-	{
-		// Decode a byte buffer into a CharBuffer
-		CharBuffer isodcb = null;
-		CharsetDecoder isodecoder = charset.newDecoder();
-		try {
-			isodcb = isodecoder.decode(buf);
-		} catch (CharacterCodingException e) {
-			log.error(e);
-		}
-		return (CharSequence)isodcb;
-	}
-	
-	private ByteBuffer encode(String str)
-	{
-		// Encode a string into a byte buffer
-		ByteBuffer bb = null;
-		CharsetEncoder isoencoder = charset.newEncoder();
-		try {
-			bb = isoencoder.encode(CharBuffer.wrap(str));
-		} catch (CharacterCodingException e) {
-			log.error(e);
-		}
-		return bb;
-	}
+  public RegexTextInboundAdapter(AdapterDefinition adapterDefinition) throws ComponentException
+  {
+    super(adapterDefinition);
+  }
+
+  @Override
+  public void afterPropertiesSet()
+  {
+    super.afterPropertiesSet();
+    // grab references to two properties in the definition that will be used to
+    // adapt the incoming bytes
+    if (hasProperty(ATTRIBUTE_SEPARATOR_TO_BE_REPLACED_PROPERTY_NAME))
+      separatorToBeReplaced = (String) getProperty(ATTRIBUTE_SEPARATOR_TO_BE_REPLACED_PROPERTY_NAME).getValue();
+    if (hasProperty(ATTRIBUTE_SEPARATOR_PROPERTY_NAME))
+      attributeSeparator = (String) getProperty(ATTRIBUTE_SEPARATOR_PROPERTY_NAME).getValue();
+  }
+
+  @Override
+  public void receive(ByteBuffer buf, String str)
+  {
+    ByteBuffer bb = buf;
+    // Check first whether we need to do anything different from the out-of-the-box
+    // TextAdapter
+    if (StringUtils.isNotBlank(separatorToBeReplaced))
+    {
+      // Decode the buffer
+      CharSequence charseq = decode(buf);
+      // Replace the attribute separator with a simple one
+      LOGGER.trace("Replacing separator {0} with {1}", separatorToBeReplaced, attributeSeparator);
+      Pattern p = Pattern.compile(separatorToBeReplaced);
+      Matcher m = p.matcher(charseq);
+      String newString = m.replaceAll(attributeSeparator);
+
+      LOGGER.trace("Updated string: {0}", newString);
+      // Re-encode the string into a byte buffer
+      bb = encode(newString);
+    }
+    // Let the base class now handle the input as normal
+    super.receive(bb, str);
+  }
+
+  private CharSequence decode(ByteBuffer buf)
+  {
+    // Decode a byte buffer into a CharBuffer
+    CharBuffer isodcb = null;
+    CharsetDecoder isodecoder = charset.newDecoder();
+    try
+    {
+      isodcb = isodecoder.decode(buf);
+    }
+    catch (CharacterCodingException e)
+    {
+      if (LOGGER.isDebugEnabled())
+        LOGGER.warn(" buffer: {0}", e, buf);
+      else
+        LOGGER.warn(" buffer: {0}", buf);
+    }
+    return (CharSequence) isodcb;
+  }
+
+  private ByteBuffer encode(String str)
+  {
+    // Encode a string into a byte buffer
+    ByteBuffer bb = null;
+    CharsetEncoder isoencoder = charset.newEncoder();
+    try
+    {
+      bb = isoencoder.encode(CharBuffer.wrap(str));
+    }
+    catch (CharacterCodingException e)
+    {
+      if (LOGGER.isDebugEnabled())
+        LOGGER.warn("Failed to encode buffer: {0}", e, str);
+      else
+        LOGGER.warn("Failed to encode buffer: {0}", str);
+    }
+    return bb;
+  }
 }
--- geoevent-components/solutions-geoevent/adapters/regexText-adapter/src/main/java/com/esri/geoevent/solutions/adapter/regexText/RegexTextInboundAdapterService.java
+++ geoevent-components/solutions-geoevent/adapters/regexText-adapter/src/main/java/com/esri/geoevent/solutions/adapter/regexText/RegexTextInboundAdapterService.java
@@ -16,21 +16,18 @@
 package com.esri.geoevent.solutions.adapter.regexText;
 
 import com.esri.ges.adapter.Adapter;
-import com.esri.ges.core.component.ComponentException;
 import com.esri.ges.adapter.text.TextInboundAdapterService;
 import com.esri.ges.adapter.util.XmlAdapterDefinition;
+import com.esri.ges.core.component.ComponentException;
 
-public class RegexTextInboundAdapterService extends TextInboundAdapterService
-{
-	public RegexTextInboundAdapterService()
-	{
-	  super();
-	  definition = new XmlAdapterDefinition(getResourceAsStream("inboundadapter-definition.xml"));
+public class RegexTextInboundAdapterService extends TextInboundAdapterService {
+	public RegexTextInboundAdapterService() {
+		super();
+		definition = new XmlAdapterDefinition(getResourceAsStream("inboundadapter-definition.xml"));
 	}
-	
+
 	@Override
-	public Adapter createAdapter() throws ComponentException
-	{
-	  return new RegexTextInboundAdapter(definition);
+	public Adapter createAdapter() throws ComponentException {
+		return new RegexTextInboundAdapter(definition);
 	}
 }
